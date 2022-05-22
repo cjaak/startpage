c71048f503ce16d4536a764085250cae8114c04e
charlie <charlie.oliver.wiegand@gmail.com>
2022-05-22 19:53:24 +0200

    feat: responsive css and filled bookmarks

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 const NAME = "Charlie";
 
+const BOOKMARK_MAP = [
+    {
+        "section": "Studies",
+        "items":[
+            {"name": "Item 1", "url": "https://google.com/?q=1"},
+            {"name": "Item 2", "url": "https://google.com/?q=2"},
+            {"name": "Item 3", "url": "https://google.com/?q=3"}
+        ]
+    },
+    {
+        "section": "Work",
+        "items":[
+            {"name": "Item 1", "url": "https://google.com/?q=1"},
+            {"name": "Item 2", "url": "https://google.com/?q=2"},
+            {"name": "Item 3", "url": "https://google.com/?q=3"}
+        ]
+    },
+    {
+        "section": "Personal",
+        "items":[
+            {"name": "Item 1", "url": "https://google.com/?q=1"},
+            {"name": "Item 2", "url": "https://google.com/?q=2"},
+            {"name": "Item 3", "url": "https://google.com/?q=3"}
+        ]
+    }
+]
+
+
+
 
 function showWelcomeMessage(){
     let greeting = `Hello ${NAME}`
@@ -9,7 +38,26 @@ function showWelcomeMessage(){
 
 function showContent(){
     let container = document.getElementById("content");
-    container.innerHTML = "Content Content Content"
+    for(const curSection of BOOKMARK_MAP){
+        let section = document.createElement("div");
+        section.className = "section";
+        container.appendChild(section);
+
+        let title = document.createElement("h1");
+        title.innerHTML = curSection.section;
+        section.appendChild(title);
+
+        for (const curItem of curSection.items){
+            let paragraph = document.createElement("p");
+            section.appendChild(paragraph);
+
+            let link = document.createElement("a");
+            link.innerHTML = curItem.name;
+            link.setAttribute("href", curItem.url);
+
+            paragraph.appendChild(link);
+        }
+    }
 
 }
 
